9b305bf0d242dd7c5a126e19b54c1363068cbbb6
Carlos_Hernandez <carlosandrehernandez@hotmail.com>
2020-08-19 12:57:00 -0700

    3.46 Passing Method References

--- src/App.js
+++ src/App.js
@@ -13,28 +13,35 @@ class App extends Component{
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log("was clicked!");
     //this.state.persons[0].name = 'Andreius'
     this.setState({
       persons: [
-        {name: 'Andre', age: 23},
+        {name: newName, age: 23},
         {name: 'Vanessa', age: 20},
-        {name: 'Ziggy', age: 4}
+        {name: 'Ziggy', age: 3}
       ]
-    
     })
   }
+
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is a paragraph</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('ANDRE!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Dre!!')}>I like Andre</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
      );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This is a heading'));
