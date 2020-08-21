b50b42795d1bfae13fba7ac968f1558eab479d0f
Carlos_Hernandez <carlosandrehernandez@hotmail.com>
2020-08-21 13:06:22 -0700

    4.56 Outputting Lists

--- src/App.js
+++ src/App.js
@@ -42,27 +42,35 @@ class App extends Component{
   }
 
   render() {
+
+    const style = {
+      backgroundCOlor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name}
+              age={person.age}/>
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is a paragraph</p>
-        <button onClick={this.togglePersonsHandler}>Switch Name</button>
-        { 
-          this.state.showPersons === true ? 
-            <div>
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age}/>
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Dre!!')}
-                changed={this.nameChangedHandler}>I like Andre</Person>
-              <Person 
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age}/>
-            </div> : null
-        }
+        <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
+        {persons}
       </div>
      );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This is a heading'));
