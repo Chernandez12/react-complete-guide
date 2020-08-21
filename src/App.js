8e47cde628316673abe042009f36c6e1de885021
Carlos_Hernandez <carlosandrehernandez@hotmail.com>
2020-08-21 13:39:00 -0700

    4.60 Flexible Components

--- src/App.js
+++ src/App.js
@@ -21,14 +21,22 @@ class App extends Component{
     this.setState({persons: persons})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Andre', age: 28 },
-        { name: event.target.value, age: 28 },
-        { name: 'Ziggy', age: 28 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id  === id;
+    });  
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
   } 
 
   togglePersonsHandler = () => {
@@ -56,7 +64,8 @@ class App extends Component{
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={index}/>
+              key={index}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       );
