f76c48aaae798ea03603782ea8c870df15618aa7
Carlos_Hernandez <carlosandrehernandez@hotmail.com>
2020-08-24 13:47:52 -0700

    5.69 Using Radium for Media Queries

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium, { StyleRoot } from 'radium';
 import './App.css';
 import Person from './Person/Person';
 //import { render } from '@testing-library/react';
@@ -45,14 +46,17 @@ class App extends Component{
   }
 
   render() {
-
     const style = {
       backgroundColor: 'green',
       color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -71,6 +75,10 @@ class App extends Component{
         </div>
       );
       style.backgroundColor = 'maroon';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -82,16 +90,18 @@ class App extends Component{
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is a paragraph</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is a paragraph</p>
+          <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
+          {persons}
+        </div>
+      </StyleRoot>
      );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This is a heading'));
     //return React.createElement('div', {className: 'App'}, 'This is not');
   }
 }
 
-export default App;
+export default Radium(App);
